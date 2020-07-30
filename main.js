e62f1f6f437246e658685b25391f78781b436d2b
小川智裕 <tomo.ogawa18@gmail..com>
2020-07-30 23:43:46 +0900

    main変更

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ const path = require('path');
 const port = 3000;
 const app = express();
 
-app.get("");
+app.get('/:aaa', function (req, res) {
+    res.send('Hello ' + req.query.name);
+  })
 
 app.listen(port, () => {
   console.log('Running at Port 3000...');
 });
 
 // 静的ファイルのルーティング
-app.use(express.static(path.join(__dirname, 'public')));å
+app.use(express.static(path.join(__dirname, 'public')));
 
 // その他のリクエストに対する404エラー
 app.use((req, res) => {
